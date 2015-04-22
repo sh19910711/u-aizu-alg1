5978ce3a03681d72466822e9706bac6c4725c04e
Hiroyuki Sano <sh19910711@gmail.com>
2015-04-22 12:55:24 +0900

    ex03: fix tests

--- ex03/s1190235-3_test.cpp
+++ ex03/s1190235-3_test.cpp
@@ -4,26 +4,27 @@ namespace {
 
 #include "./s1190235-3.c"
 
-TEST(ProblemThree, PushAndPop) {
-  stack_init();
-  stack_push('1');
-  stack_push('2');
-  stack_push('+');
-  ASSERT_EQ(stack_pop(), '+');
-  ASSERT_EQ(stack_pop(), '2');
-  ASSERT_EQ(stack_pop(), '1');
-}
+  TEST(ProblemThree, PushAndPop) {
+    stack_init();
+    stack_push(1);
+    stack_push(2);
+    stack_push(3);
+    ASSERT_EQ(stack_pop(), 3);
+    ASSERT_EQ(stack_pop(), 2);
+    ASSERT_EQ(stack_pop(), 1);
+    stack_finalize();
+  }
 
-TEST(ProblemThree, OnePlusTwo) {
-  ASSERT_EQ(eval("12+"), 3);
-}
+  TEST(ProblemThree, OnePlusTwo) {
+    ASSERT_EQ(eval("12+"), 3);
+  }
 
-TEST(ProblemThree, OneTimesTwo) {
-  ASSERT_EQ(eval("12*"), 2);
-}
+  TEST(ProblemThree, OneTimesTwo) {
+    ASSERT_EQ(eval("12*"), 2);
+  }
 
-TEST(ProblemThree, ExampleInput) {
-  ASSERT_EQ(eval("12+34+*"), 21);
-}
+  TEST(ProblemThree, ExampleInput) {
+    ASSERT_EQ(eval("12+34+*"), 21);
+  }
 
 }
