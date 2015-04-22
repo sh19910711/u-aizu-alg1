f0317a20ebb5d1cde8a0736944f362c3f7aefa7b
Hiroyuki Sano <sh19910711@gmail.com>
2015-04-22 13:34:10 +0900

    fix tests

--- ex03/s1190235-0_test.cpp
+++ ex03/s1190235-0_test.cpp
@@ -1,6 +1,11 @@
 #include <gtest/gtest.h>
+
+namespace {
+
 #include "./s1190235-0.c"
 
-TEST(HelloTest, CallFunction) {
-  EXPECT_STREQ("hello, world", get_hello());
+  TEST(HelloTest, CallFunction) {
+    ASSERT_STREQ("hello, world", get_hello());
+  }
+
 }
