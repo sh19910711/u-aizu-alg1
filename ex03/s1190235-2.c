ad75f28d3d9bb3bb8341a051d52456f5753592b1
Hiroyuki Sano <sh19910711@gmail.com>
2015-04-22 12:08:57 +0900

    ex03: add eval()

--- ex03/s1190235-2.c
+++ ex03/s1190235-2.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
 
 /* 問題２ */
 /* スタックを作るのには配列を用いる */ 
@@ -38,6 +41,44 @@ void stack_print() {
     }
   }
   printf("\n");
+}
+
+void eval_print() {
+#ifndef GTEST_INCLUDE_GTEST_GTEST_H_
+  stack_print();
+#endif
+}
+
+int to_int(char c) {
+  assert(isdigit(c));
+  return c - '0';
+}
+
+int eval(char* s) {
+  int i;
+  int len;
+  int a;
+  int b;
+
+  len = strlen(s);
+
+  for ( i = 0; i < len; ++ i ) {
+    if ( isdigit(s[i]) ) {
+      stack_push(to_int(s[i]));
+    } else {
+      a = stack_pop();
+      b = stack_pop();
+      if ( s[i] == '+' ) {
+        stack_push(a + b);
+      } else if ( s[i] == '-' ) {
+        stack_push(a - b);
+      } else if ( s[i] == '*' ) {
+        stack_push(a * b);
+      }
+    }
+    eval_print();
+  }
+  return stack_pop();
 }
 
 #ifndef GTEST_INCLUDE_GTEST_GTEST_H_
