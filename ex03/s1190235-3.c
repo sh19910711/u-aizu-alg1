5831434c3555efeee3194f16ad5f2ed58a931675
Hiroyuki Sano <sh19910711@gmail.com>
2015-04-22 12:54:46 +0900

    ex03: add stack

--- ex03/s1190235-3.c
+++ ex03/s1190235-3.c
@@ -7,16 +7,62 @@
 /* ただし、スタックを作るのにはリストを用いること。 */
 /* 他の条件については、問題２と同じ */
 
+typedef int NodeValue;
+struct Node {
+  NodeValue value;
+  struct Node* next;
+};
+
+struct Node* head;
+struct Node* tail;
+
+struct Node* create_node() {
+  return (struct Node*)malloc(sizeof(struct Node));
+}
+
+void push_front(NodeValue value) {
+  struct Node* new_node = create_node();
+  new_node->value = value;
+  new_node->next = head->next;
+  head->next = new_node;
+}
+
+NodeValue pop_front() {
+  NodeValue res;
+  struct Node* new_head_next;
+
+  assert(head->next != tail);
+
+  new_head_next = head->next->next;
+  res = head->next->value;
+  free(head->next);
+  head->next = new_head_next;
+
+  return res;
+}
+
 int stack_empty() {
+  return head->next == tail;
 }
 
 void stack_init() {
+  head = create_node();
+  tail = create_node();
+  head->next = tail;
+  tail->next = tail;
 }
 
-void stack_push(char c) {
+void stack_finalize() {
+  free(head);
+  free(tail);
+}
+
+void stack_push(int x) {
+  push_front(x);
 }
 
 char stack_pop() {
+  return pop_front();
 }
 
 void stack_print() {
@@ -34,11 +80,13 @@ int to_int(char c) {
 }
 
 int eval(char* s) {
+  int res;
   int i;
   int len;
   int a;
   int b;
 
+  stack_init();
   len = strlen(s);
 
   for ( i = 0; i < len; ++ i ) {
@@ -57,7 +105,10 @@ int eval(char* s) {
     }
     eval_print();
   }
-  return stack_pop();
+
+  res = stack_pop();
+  stack_finalize();
+  return res;
 }
 
 #ifndef GTEST_INCLUDE_GTEST_GTEST_H_
