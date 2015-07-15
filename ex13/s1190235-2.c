46b722d861e13838b7b23b5304aeef4155300136
Hiroyuki Sano <sh19910711@gmail.com>
2015-07-15 09:23:32 +0900

    ex13: init project

---
+++ ex13/s1190235-2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+#ifndef GTEST_INCLUDE_GTEST_GTEST_H_
+int main() {
+  return 0;
+}
+#endif
---
+++ ex13/s1190235-2_test.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+
+namespace {
+
+#include "./s1190235-2.c"
+
+}
